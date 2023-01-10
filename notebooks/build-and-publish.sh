704f7a8335022ba06e96b26d6450cac767991293
rmshkv <eromashkova@crhtc45.hpc.ucar.edu>
2023-01-10 15:20:33 -0700

    Added test MOC diagnostic, with computation, basic caching, and visualization steps

--- notebooks/build-and-publish.sh
+++ notebooks/build-and-publish.sh
@@ -2,14 +2,17 @@
 
 set -e
 
+casename=gcp-cases
+
 remote_mach=thorodin.cgd.ucar.edu
-remote_dir=/web/web-data/staff/mclong/my-cesm-experiment
+remote_dir=/project/webshare/projects/ocean-diagnostics-books/${USER}/${casename}
 
 source activate cesm-exp
 
 conda info --envs
 
 jupyter-book clean _computed-notebooks
-jupyter-book build _computed-notebooks --all
+jupyter-book build _computed-notebooks/*casename --all
 
-scp -r _computed-notebooks/_build/html/* ${remote_mach}:${remote_dir}
+ssh ${USER}@${remote_mach} "mkdir -p :${remote_dir}"
+scp -r _computed-notebooks/*casename/_build/html/* ${remote_mach}:${remote_dir}
